50a04a7c1bb6b08772784d4a2e45a7003738b3fa
Bruno Moleta <brunomoleta@pm.me>
2024-04-26 13:07:04 -0300

    🧹 Formatação de código com Prettier

--- cypress/contracts/produtos.contract.js
+++ cypress/contracts/produtos.contract.js
@@ -1,13 +1,13 @@
-const Joi = require ('joi')
+const Joi = require("joi");
 
 const produtosSchema = Joi.object({
-    quantidade: Joi.number(), 
-    produtos: Joi.array().items({
-        nome: Joi.string(),
-        preco: Joi.number(),
-        descricao: Joi.string(),
-        quantidade: Joi.number(),
-        _id: Joi.string()
-    })
-})
+  quantidade: Joi.number(),
+  produtos: Joi.array().items({
+    nome: Joi.string().min(1).required(),
+    preco: Joi.number().greater(0).required(),
+    descricao: Joi.string().min(1).required(),
+    quantidade: Joi.number().min(1).required(),
+    _id: Joi.string().required(),
+  }),
+});
 export default produtosSchema;
