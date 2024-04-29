5b5d5949567c32280bd8d70e5ae6891b4b04ed95
Bruno Moleta <brunomoleta@pm.me>
2024-04-29 17:01:29 -0300

    :fix: cypress baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3000/",
+    baseUrl: "http://lojaebac.ebaconline.art.br/",
+    execTimeout: 8 * 1000,
   },
 });
